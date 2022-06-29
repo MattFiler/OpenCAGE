a95f6792ed7896254d6d679e4bfab488779408ec
MattFiler <matthew.filer@hotmail.co.uk>
2022-06-29 20:01:50 +0100

    including hashes in manifest

--- Source/Packager/Program.cs
+++ Source/Packager/Program.cs
@@ -3,14 +3,15 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace Packager
 {
     class Program
     {
-        private static JArray manifest_files = new JArray();
-        private static string output_path = "../../Assets/";
-        private static List<string> all_archives = new List<string>();
+        private static string _outputPath = "../../Assets/";
+        private static JArray _archiveMetadatas = new JArray();
+        private static List<string> _archiveFiles = new List<string>();
 
         /*
          * 
@@ -36,33 +37,33 @@ namespace Packager
             //END OF LIST
 
             Console.WriteLine("PACKAGER: Saving manifest.");
-            JObject manifest_config = JObject.Parse("{}");
-            manifest_config["archives"] = manifest_files;
-            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + output_path + "assets.manifest", manifest_config.ToString(Formatting.Indented));
+            JObject manifest = JObject.Parse("{}");
+            manifest["archives"] = _archiveMetadatas;
+            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + _outputPath + "assets.manifest", manifest.ToString(Formatting.Indented));
 
-            string[] files = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + output_path, "*.*", SearchOption.AllDirectories);
+            string[] files = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + _outputPath, "*.*", SearchOption.AllDirectories);
             foreach (string file in files)
             {
-                string this_filename = Path.GetFileNameWithoutExtension(file);
-                if (this_filename != "assets" && !all_archives.Contains(this_filename)) File.Delete(file);
+                string fileName = Path.GetFileNameWithoutExtension(file);
+                if (fileName != "assets" && !_archiveFiles.Contains(fileName)) File.Delete(file);
             }
         }
 
-        static void WriteFilesToArchive(string originalPath, string outputFilename)
+        static void WriteFilesToArchive(string originalPath, string archiveName)
         {
-            Console.WriteLine("PACKAGER: Creating archive: " + outputFilename);
-            all_archives.Add(outputFilename);
+            Console.WriteLine("PACKAGER: Creating archive: " + archiveName);
+            _archiveFiles.Add(archiveName);
 
             string exceptionsFile = "OPENCAGE_EXCEPTIONS";
             string folderPath = AppDomain.CurrentDomain.BaseDirectory + "../../" + originalPath;
-            string filenameWithoutExtension = AppDomain.CurrentDomain.BaseDirectory + output_path + outputFilename;
 
             List<string> fileExceptions = new List<string>();
             if (File.Exists(folderPath + exceptionsFile)) fileExceptions.AddRange(File.ReadAllLines(folderPath + exceptionsFile));
             fileExceptions.Add(exceptionsFile);
 
+            string archivePath = AppDomain.CurrentDomain.BaseDirectory + _outputPath + archiveName + ".archive";
             string[] files = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories);
-            BinaryWriter writer = new BinaryWriter(File.OpenWrite(filenameWithoutExtension + "_temp.archive"));
+            BinaryWriter writer = new BinaryWriter(File.OpenWrite(archivePath));
             writer.BaseStream.SetLength(0);
             writer.Write(0);
             int writeCount = 0;
@@ -70,31 +71,24 @@ namespace Packager
             {
                 string filepathLocal = file.Replace(folderPath, "");
                 if (fileExceptions.Contains(filepathLocal)) continue;
-                byte[] this_file = File.ReadAllBytes(file);
-                writer.Write(outputFilename + "/" + filepathLocal);
-                writer.Write(this_file.Length);
-                writer.Write(this_file);
+                byte[] fileContent = File.ReadAllBytes(file);
+                writer.Write(archiveName + "/" + filepathLocal);
+                writer.Write(fileContent.Length);
+                writer.Write(fileContent);
                 writeCount++;
             }
             writer.BaseStream.Position = 0;
             writer.Write(writeCount);
-            int file_length = (int)writer.BaseStream.Length;
+            int archiveSize = (int)writer.BaseStream.Length;
             writer.Close();
 
-            FileInfo oldArchive = null;
-            if (File.Exists(filenameWithoutExtension + ".archive")) oldArchive = new FileInfo(filenameWithoutExtension + ".archive");
-            FileInfo newArchive = new FileInfo(filenameWithoutExtension + "_temp.archive");
-            if (oldArchive == null || oldArchive.Length != newArchive.Length)
-            {
-                if (File.Exists(filenameWithoutExtension + ".archive")) File.Delete(filenameWithoutExtension + ".archive");
-                File.Move(filenameWithoutExtension + "_temp.archive", filenameWithoutExtension + ".archive");
-            }
-            else
-            {
-                File.Delete(filenameWithoutExtension + "_temp.archive");
-            }
+            MD5 md5 = MD5.Create();
+            byte[] contentBytes = File.ReadAllBytes(archivePath);
+            md5.TransformBlock(contentBytes, 0, contentBytes.Length, contentBytes, 0);
+            md5.TransformFinalBlock(new byte[0], 0, 0);
+            string archiveHash = BitConverter.ToString(md5.Hash).Replace("-", "").ToLower();
 
-            manifest_files.Add(JObject.Parse("{\"name\":\"" + outputFilename + "\",\"size\":\"" + file_length + "\"}"));
+            _archiveMetadatas.Add(JObject.Parse("{\"name\":\"" + archiveName + "\",\"size\":\"" + archiveSize + "\",\"hash\":\"" + archiveHash + "\"}"));
         }
     }
 }
