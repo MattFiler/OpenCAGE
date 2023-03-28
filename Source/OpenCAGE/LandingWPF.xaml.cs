1f5ed583d4352955338d65aa64480924716b7f65
MattFiler <matthew.filer@hotmail.co.uk>
2023-03-28 09:22:46 +0100

    redesigning landing page through WPF

---
+++ Source/OpenCAGE/LandingWPF.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OpenCAGE
+{
+    /// <summary>
+    /// Interaction logic for LandingWPF.xaml
+    /// </summary>
+    public partial class LandingWPF : UserControl
+    {
+        public Action<Process> OnToolOpened;
+        public Action<Process> OnToolClosed;
+
+        public Action OnUpdateRequest;
+        public Action OnSettingsRequest;
+
+        public LandingWPF()
+        {
+            InitializeComponent();
+        }
+        public void SetVersionInfo(string version)
+        {
+            BranchText.Content = " [" + SettingsManager.GetString("CONFIG_RemoteBranch") + "]";
+            VersionText.Content = "Version " + version;
+        }
+
+        /* App launch buttons */
+        private void OpenAssetEditor(object sender, RoutedEventArgs e)
+        {
+            StartProcess("asseteditor/AlienPAK.exe");
+        }
+        private void OpenConfigurationEditor(object sender, RoutedEventArgs e)
+        {
+            StartProcess("configeditor/AlienConfigEditor.exe");
+        }
+        private void OpenScriptEditor(object sender, RoutedEventArgs e)
+        {
+            StartProcess("scripteditor/CommandsEditor.exe");
+        }
+        private void OpenBehaviourEditor(object sender, RoutedEventArgs e)
+        {
+            StartProcess("legendplugin/BehaviourTreeTool.exe");
+        }
+        private void OpenGameLauncher(object sender, RoutedEventArgs e)
+        {
+            StartProcess("launchgame/LaunchGame.exe");
+        }
+
+        private void GithubButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void SettingsButtonClick(object sender, RoutedEventArgs e)
+        {
+            OnSettingsRequest?.Invoke();
+        }
+
+        /* Start a process from the remote directory */
+        private void StartProcess(string path)
+        {
+            string pathToExe = SettingsManager.GetString("PATH_GameRoot") + "/DATA/MODTOOLS/REMOTE_ASSETS/" + path;
+            if (!File.Exists(pathToExe))
+            {
+                OnUpdateRequest?.Invoke();
+                return;
+            }
+
+            Process process = new Process();
+            process.Exited += Process_Exited;
+            process.StartInfo.FileName = pathToExe;
+            process.StartInfo.Arguments = "-opencage " + SettingsManager.GetString("PATH_GameRoot");
+            process.StartInfo.WorkingDirectory = pathToExe.Substring(0, pathToExe.Length - System.IO.Path.GetFileName(pathToExe).Length);
+            process.Start();
+
+            OnToolOpened?.Invoke(process);
+        }
+        private void Process_Exited(object sender, EventArgs e)
+        {
+            OnToolClosed?.Invoke((Process)sender);
+        }
+    }
+}
