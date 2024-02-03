4457653cd50f2809a1615858a2d3eeb41cd26158
MattFiler <matthew.filer@hotmail.co.uk>
2024-02-03 13:39:35 +0000

    synced latest updates

--- Source/OpenCAGE/Landing.Designer.cs
+++ Source/OpenCAGE/Landing.Designer.cs
@@ -50,7 +50,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1326, 528);
             this.Controls.Add(this.elementHost1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Landing";
