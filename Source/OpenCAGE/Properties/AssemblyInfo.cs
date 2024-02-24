e7acfbd927245780ed2556edfe050f57fa6e12de
Matt Filer <matthew.filer@hotmail.co.uk>
2024-02-24 09:43:54 +0000

    updated script editor

--- Source/OpenCAGE/Properties/AssemblyInfo.cs
+++ Source/OpenCAGE/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.12.0.02")]
-[assembly: AssemblyFileVersion("0.12.0.02")]
+[assembly: AssemblyFileVersion("0.12.0.1")]
