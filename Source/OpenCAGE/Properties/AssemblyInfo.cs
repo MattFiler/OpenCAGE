be81d9d7804ac9b864202270fc1b148222e03f9e
Matt Filer <matthew.filer@hotmail.co.uk>
2025-07-11 10:03:43 +0100

    bump script editor

--- Source/OpenCAGE/Properties/AssemblyInfo.cs
+++ Source/OpenCAGE/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.14.0.07")]
-[assembly: AssemblyFileVersion("0.14.0.07")]
+[assembly: AssemblyVersion("0.14.0.08")]
+[assembly: AssemblyFileVersion("0.14.0.08")]
