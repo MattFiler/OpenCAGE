cd49a9e64c77e07fe2cd895decad536bcaa1f885
Matt Filer <matthew.filer@hotmail.co.uk>
2026-05-12 21:06:17 +0100

    add constant ambient patch

--- Source/OpenCAGE/Popups/LaunchGame.Designer.cs
+++ Source/OpenCAGE/Popups/LaunchGame.Designer.cs
@@ -46,16 +46,16 @@
             this.levelList = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.renderConstantAmbient = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // OpenGame
             // 
             this.OpenGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.OpenGame.Location = new System.Drawing.Point(22, 354);
-            this.OpenGame.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.OpenGame.Location = new System.Drawing.Point(15, 230);
             this.OpenGame.Name = "OpenGame";
-            this.OpenGame.Size = new System.Drawing.Size(525, 68);
+            this.OpenGame.Size = new System.Drawing.Size(350, 44);
             this.OpenGame.TabIndex = 22;
             this.OpenGame.Text = "Start Alien: Isolation";
             this.OpenGame.UseVisualStyleBackColor = true;
@@ -65,10 +65,9 @@
             // 
             this.enableRuntimeUtils.AutoSize = true;
             this.enableRuntimeUtils.Enabled = false;
-            this.enableRuntimeUtils.Location = new System.Drawing.Point(286, 105);
-            this.enableRuntimeUtils.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.enableRuntimeUtils.Location = new System.Drawing.Point(191, 68);
             this.enableRuntimeUtils.Name = "enableRuntimeUtils";
-            this.enableRuntimeUtils.Size = new System.Drawing.Size(184, 24);
+            this.enableRuntimeUtils.Size = new System.Drawing.Size(130, 17);
             this.enableRuntimeUtils.TabIndex = 30;
             this.enableRuntimeUtils.Text = "Enable Hot Reloading";
             this.toolTip1.SetToolTip(this.enableRuntimeUtils, "Press INSERT in-game to hot reload the current level.");
@@ -78,10 +77,9 @@
             // enableCinematicTools
             // 
             this.enableCinematicTools.AutoSize = true;
-            this.enableCinematicTools.Location = new System.Drawing.Point(286, 140);
-            this.enableCinematicTools.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.enableCinematicTools.Location = new System.Drawing.Point(191, 91);
             this.enableCinematicTools.Name = "enableCinematicTools";
-            this.enableCinematicTools.Size = new System.Drawing.Size(194, 24);
+            this.enableCinematicTools.Size = new System.Drawing.Size(137, 17);
             this.enableCinematicTools.TabIndex = 27;
             this.enableCinematicTools.Text = "Enable Cinematic Tools";
             this.toolTip1.SetToolTip(this.enableCinematicTools, "Enables HattiWatti\'s Cinematic Tools.");
@@ -91,10 +89,9 @@
             // enableUIPerf
             // 
             this.enableUIPerf.AutoSize = true;
-            this.enableUIPerf.Location = new System.Drawing.Point(286, 34);
-            this.enableUIPerf.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.enableUIPerf.Location = new System.Drawing.Point(191, 22);
             this.enableUIPerf.Name = "enableUIPerf";
-            this.enableUIPerf.Size = new System.Drawing.Size(215, 24);
+            this.enableUIPerf.Size = new System.Drawing.Size(152, 17);
             this.enableUIPerf.TabIndex = 28;
             this.enableUIPerf.Text = "Enable UI Memory Overlay";
             this.toolTip1.SetToolTip(this.enableUIPerf, "Displays current UI memory in an in-game overlay.");
@@ -104,10 +101,9 @@
             // enableMemReplayLogs
             // 
             this.enableMemReplayLogs.AutoSize = true;
-            this.enableMemReplayLogs.Location = new System.Drawing.Point(286, 69);
-            this.enableMemReplayLogs.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.enableMemReplayLogs.Location = new System.Drawing.Point(191, 45);
             this.enableMemReplayLogs.Name = "enableMemReplayLogs";
-            this.enableMemReplayLogs.Size = new System.Drawing.Size(199, 24);
+            this.enableMemReplayLogs.Size = new System.Drawing.Size(140, 17);
             this.enableMemReplayLogs.TabIndex = 29;
             this.enableMemReplayLogs.Text = "Enable Memory Logging";
             this.toolTip1.SetToolTip(this.enableMemReplayLogs, "Enables memory replay logs, saved to the Mem_Replay_Logs folder.");
@@ -117,10 +113,9 @@
             // disableUI
             // 
             this.disableUI.AutoSize = true;
-            this.disableUI.Location = new System.Drawing.Point(286, 175);
-            this.disableUI.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.disableUI.Location = new System.Drawing.Point(191, 114);
             this.disableUI.Name = "disableUI";
-            this.disableUI.Size = new System.Drawing.Size(69, 24);
+            this.disableUI.Size = new System.Drawing.Size(54, 17);
             this.disableUI.TabIndex = 31;
             this.disableUI.Text = "No UI";
             this.toolTip1.SetToolTip(this.disableUI, "Enabling this will disable the in-game HUD. Be aware that this also disables UI p" +
@@ -131,10 +126,9 @@
             // skipFrontend
             // 
             this.skipFrontend.AutoSize = true;
-            this.skipFrontend.Location = new System.Drawing.Point(16, 175);
-            this.skipFrontend.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.skipFrontend.Location = new System.Drawing.Point(11, 114);
             this.skipFrontend.Name = "skipFrontend";
-            this.skipFrontend.Size = new System.Drawing.Size(128, 24);
+            this.skipFrontend.Size = new System.Drawing.Size(92, 17);
             this.skipFrontend.TabIndex = 32;
             this.skipFrontend.Text = "Skip Frontend";
             this.toolTip1.SetToolTip(this.skipFrontend, "Enabling this will skip the title screen when loading directly to a level.");
@@ -144,10 +138,9 @@
             // patchCurrentGen
             // 
             this.patchCurrentGen.AutoSize = true;
-            this.patchCurrentGen.Location = new System.Drawing.Point(16, 211);
-            this.patchCurrentGen.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.patchCurrentGen.Location = new System.Drawing.Point(11, 137);
             this.patchCurrentGen.Name = "patchCurrentGen";
-            this.patchCurrentGen.Size = new System.Drawing.Size(262, 24);
+            this.patchCurrentGen.Size = new System.Drawing.Size(179, 17);
             this.patchCurrentGen.TabIndex = 33;
             this.patchCurrentGen.Text = "Patch Current-Gen Optimisations";
             this.toolTip1.SetToolTip(this.patchCurrentGen, "Enabling this will patch current-gen script optimisations to enable functionality" +
@@ -158,10 +151,9 @@
             // UIMOD_DebugCheckpoints
             // 
             this.UIMOD_DebugCheckpoints.AutoSize = true;
-            this.UIMOD_DebugCheckpoints.Location = new System.Drawing.Point(16, 34);
-            this.UIMOD_DebugCheckpoints.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.UIMOD_DebugCheckpoints.Location = new System.Drawing.Point(11, 22);
             this.UIMOD_DebugCheckpoints.Name = "UIMOD_DebugCheckpoints";
-            this.UIMOD_DebugCheckpoints.Size = new System.Drawing.Size(222, 24);
+            this.UIMOD_DebugCheckpoints.Size = new System.Drawing.Size(156, 17);
             this.UIMOD_DebugCheckpoints.TabIndex = 23;
             this.UIMOD_DebugCheckpoints.Text = "Enable Debug Checkpoints";
             this.UIMOD_DebugCheckpoints.UseVisualStyleBackColor = true;
@@ -170,10 +162,9 @@
             // UIMOD_MapSelection
             // 
             this.UIMOD_MapSelection.AutoSize = true;
-            this.UIMOD_MapSelection.Location = new System.Drawing.Point(16, 69);
-            this.UIMOD_MapSelection.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.UIMOD_MapSelection.Location = new System.Drawing.Point(11, 45);
             this.UIMOD_MapSelection.Name = "UIMOD_MapSelection";
-            this.UIMOD_MapSelection.Size = new System.Drawing.Size(189, 24);
+            this.UIMOD_MapSelection.Size = new System.Drawing.Size(135, 17);
             this.UIMOD_MapSelection.TabIndex = 24;
             this.UIMOD_MapSelection.Text = "Enable Level Selection";
             this.UIMOD_MapSelection.UseVisualStyleBackColor = true;
@@ -182,10 +173,9 @@
             // UIMOD_MapName
             // 
             this.UIMOD_MapName.AutoSize = true;
-            this.UIMOD_MapName.Location = new System.Drawing.Point(16, 105);
-            this.UIMOD_MapName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.UIMOD_MapName.Location = new System.Drawing.Point(11, 68);
             this.UIMOD_MapName.Name = "UIMOD_MapName";
-            this.UIMOD_MapName.Size = new System.Drawing.Size(218, 24);
+            this.UIMOD_MapName.Size = new System.Drawing.Size(153, 17);
             this.UIMOD_MapName.TabIndex = 25;
             this.UIMOD_MapName.Text = "Enable Debug Loadscreen";
             this.UIMOD_MapName.UseVisualStyleBackColor = true;
@@ -194,10 +184,9 @@
             // UIMOD_ReturnFrontend
             // 
             this.UIMOD_ReturnFrontend.AutoSize = true;
-            this.UIMOD_ReturnFrontend.Location = new System.Drawing.Point(16, 140);
-            this.UIMOD_ReturnFrontend.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.UIMOD_ReturnFrontend.Location = new System.Drawing.Point(11, 91);
             this.UIMOD_ReturnFrontend.Name = "UIMOD_ReturnFrontend";
-            this.UIMOD_ReturnFrontend.Size = new System.Drawing.Size(229, 24);
+            this.UIMOD_ReturnFrontend.Size = new System.Drawing.Size(162, 17);
             this.UIMOD_ReturnFrontend.TabIndex = 26;
             this.UIMOD_ReturnFrontend.Text = "Add Quit To Menu On Death";
             this.UIMOD_ReturnFrontend.UseVisualStyleBackColor = true;
@@ -207,15 +196,15 @@
             // 
             this.levelList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.levelList.FormattingEnabled = true;
-            this.levelList.Location = new System.Drawing.Point(18, 46);
-            this.levelList.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.levelList.Location = new System.Drawing.Point(12, 30);
             this.levelList.Name = "levelList";
-            this.levelList.Size = new System.Drawing.Size(523, 28);
+            this.levelList.Size = new System.Drawing.Size(350, 21);
             this.levelList.TabIndex = 30;
             this.levelList.SelectedIndexChanged += new System.EventHandler(this.levelList_SelectedIndexChanged);
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.renderConstantAmbient);
             this.groupBox1.Controls.Add(this.patchCurrentGen);
             this.groupBox1.Controls.Add(this.skipFrontend);
             this.groupBox1.Controls.Add(this.disableUI);
@@ -227,11 +216,9 @@
             this.groupBox1.Controls.Add(this.UIMOD_ReturnFrontend);
             this.groupBox1.Controls.Add(this.enableUIPerf);
             this.groupBox1.Controls.Add(this.enableCinematicTools);
-            this.groupBox1.Location = new System.Drawing.Point(18, 94);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Location = new System.Drawing.Point(12, 61);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox1.Size = new System.Drawing.Size(525, 251);
+            this.groupBox1.Size = new System.Drawing.Size(350, 163);
             this.groupBox1.TabIndex = 31;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Launch Options";
@@ -239,25 +226,35 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 18);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(12, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 20);
+            this.label1.Size = new System.Drawing.Size(72, 13);
             this.label1.TabIndex = 32;
             this.label1.Text = "Starting Level";
             // 
+            // renderConstantAmbient
+            // 
+            this.renderConstantAmbient.AutoSize = true;
+            this.renderConstantAmbient.Location = new System.Drawing.Point(191, 137);
+            this.renderConstantAmbient.Name = "renderConstantAmbient";
+            this.renderConstantAmbient.Size = new System.Drawing.Size(147, 17);
+            this.renderConstantAmbient.TabIndex = 34;
+            this.renderConstantAmbient.Text = "Render Constant Ambient";
+            this.toolTip1.SetToolTip(this.renderConstantAmbient, "Enabling this will render in \'constant ambient\' mode (essentially full-bright).");
+            this.renderConstantAmbient.UseVisualStyleBackColor = true;
+            this.renderConstantAmbient.CheckedChanged += new System.EventHandler(this.renderConstantAmbient_CheckedChanged);
+            // 
             // LaunchGame
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(561, 443);
+            this.ClientSize = new System.Drawing.Size(374, 288);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.levelList);
             this.Controls.Add(this.OpenGame);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = global::OpenCAGE.SharedFormIcon.Icon;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "LaunchGame";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -286,5 +283,6 @@
         private System.Windows.Forms.CheckBox disableUI;
         private System.Windows.Forms.CheckBox skipFrontend;
         private System.Windows.Forms.CheckBox patchCurrentGen;
+        private System.Windows.Forms.CheckBox renderConstantAmbient;
     }
 }
