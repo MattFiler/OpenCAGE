99fe1c25d89f9ff8aa1f612f5f029b3119f53d4f
Matt Filer <matthew.filer@hotmail.co.uk>
2026-07-23 21:21:46 +0100

    show tree editor in release

--- Source/OpenCAGE/CommandsEditor.cs
+++ Source/OpenCAGE/CommandsEditor.cs
@@ -148,7 +148,6 @@ namespace OpenCAGE
             localisationToolStripMenuItem.Visible = false;
             levelTextDBsToolStripMenuItem.Visible = false;
             fontConfigToolStripMenuItem.Visible = false;
-            animationTreesToolStripMenuItem.Visible = false;
 #endif
 
             WindowState = SettingsManager.GetString(Settings.WindowState, "Normal") == "Maximized" ? FormWindowState.Maximized : FormWindowState.Normal;
